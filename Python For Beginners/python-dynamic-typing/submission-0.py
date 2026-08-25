97bdc0b16003fc6f96e232122ccc3abb2f19d10b
Brett Balquist <113616657+brett-b112@users.noreply.github.com>
2026-08-24 21:38:48 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10         # int type
+print(type(variable))
+variable = 10.1         # int type
+
+print(type(variable))
+variable = True    # int type
+
+print(type(variable))
+variable = "str"         # int type
+
+print(type(variable))
+variable = [1,2,3]         # int type
+
+print(type(variable))
