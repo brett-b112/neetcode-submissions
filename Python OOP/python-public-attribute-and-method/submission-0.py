b0895ee6a46ff329cc2d9a7e8d4d27e706055d61
Brett Balquist <113616657+brett-b112@users.noreply.github.com>
2026-09-16 20:02:07 -0500

    Add: python-public-attribute-and-method - submission-0

---
+++ Python OOP/python-public-attribute-and-method/submission-0.py
@@ -0,0 +1,12 @@
+class StoreItem:
+    def __init__(self, name: str, cost: float) -> None: 
+        self.name = name
+        self.cost = cost
+
+
+chips = StoreItem("Chips", 1.99) # Don't modify this line
+print(chips.name)
+print(chips.cost)
+# TODO: Access the attributes of the chips object and display them
+
+
